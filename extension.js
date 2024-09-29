7c0604f2b166141201d0655602c417638b83395f
Artem Babii <arthem.babij@gmail.com>
2024-09-29 11:42:08 +0300

    add gnome 47 support

--- extension.js
+++ extension.js
@@ -11,231 +11,249 @@ import GLib from "gi://GLib";
 import St from "gi://St";
 
 const ICON = "alarm-symbolic";
-const SOUND = "complete";
+const TEXT = "It is time to stretch your back!";
+
+const systemSource = MessageTray.getSystemSource();
 
 const urgencyMapping = {
-    0: MessageTray.Urgency.LOW,
-    1: MessageTray.Urgency.NORMAL,
-    2: MessageTray.Urgency.CRITICAL,
+  0: MessageTray.Urgency.LOW,
+  1: MessageTray.Urgency.NORMAL,
+  2: MessageTray.Urgency.CRITICAL,
 };
 
 const DejaviewIndicator = GObject.registerClass(
-    class DejaviewIndicator extends QuickSettings.SystemIndicator {
-        _init(extensionObject) {
-            super._init();
+  class DejaviewIndicator extends QuickSettings.SystemIndicator {
+    _init(extensionObject) {
+      super._init();
 
-            this._indicator = this._addIndicator();
-            this._indicator.icon_name = ICON;
+      this._indicator = this._addIndicator();
+      this._indicator.icon_name = ICON;
 
-            this._settings = extensionObject.getSettings();
-            this._settings.bind("timer-enabled", this._indicator, "visible", Gio.SettingsBindFlags.DEFAULT);
-        }
+      this._settings = extensionObject.getSettings();
+      this._settings.bind(
+        "timer-enabled",
+        this._indicator,
+        "visible",
+        Gio.SettingsBindFlags.DEFAULT,
+      );
     }
+  },
 );
 
 const DejaviewMenuToggle = GObject.registerClass(
-    class DejaviewMenuToggle extends QuickSettings.QuickMenuToggle {
-        _init(extensionObject) {
-            super._init({
-                title: extensionObject.metadata.name,
-                gicon: Gio.Icon.new_for_string(ICON),
-                toggleMode: true,
-            });
+  class DejaviewMenuToggle extends QuickSettings.QuickMenuToggle {
+    _init(extensionObject) {
+      super._init({
+        title: extensionObject.metadata.name,
+        gicon: Gio.Icon.new_for_string(ICON),
+        toggleMode: true,
+      });
 
-            this._settings = extensionObject.getSettings();
-            this._settings.bind("timer-enabled", this, "checked", Gio.SettingsBindFlags.DEFAULT);
+      this._settings = extensionObject.getSettings();
+      this._settings.bind(
+        "timer-enabled",
+        this,
+        "checked",
+        Gio.SettingsBindFlags.DEFAULT,
+      );
 
-            this.menu.setHeader(ICON, extensionObject.metadata.name);
-            this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
-            this.menu.addAction("Settings", () => extensionObject.openPreferences(), "preferences-system-symbolic");
-        }
+      this.menu.setHeader(ICON, extensionObject.metadata.name);
+      this.menu.addMenuItem(new PopupMenu.PopupSeparatorMenuItem());
+      this.menu.addAction(
+        "Settings",
+        () => extensionObject.openPreferences(),
+        "preferences-system-symbolic",
+      );
     }
+  },
 );
 
 export default class DejaviewExtension extends Extension {
-    constructor(metadata) {
-        super(metadata);
-        this._indicator = null;
-        this._timer = null;
-        this._timerLabel = null;
-        this._notifyLoop = null;
-        this._timerSec = null;
-        this._startSec = null;
-        this._lastSec = null;
-        this._diffSec = null;
-        this._settings = null;
-        this._showTimerId = null;
-        this._closingId = null;
-        this._timerEnabledId = null;
+  constructor(metadata) {
+    super(metadata);
+    this._indicator = null;
+    this._timer = null;
+    this._timerLabel = null;
+    this._notifyLoop = null;
+    this._timerSec = null;
+    this._startSec = null;
+    this._lastSec = null;
+    this._diffSec = null;
+    this._settings = null;
+    this._showTimerId = null;
+    this._closingId = null;
+    this._timerEnabledId = null;
+  }
+
+  _notify() {
+    const messageText = this._settings.get_string("message-text") || TEXT;
+    const iconName = this._settings.get_string("icon-name") || ICON;
+    const urgencyLevel = this._settings.get_int("urgency-level");
+    const mappedUrgency = urgencyMapping[urgencyLevel];
+
+    const notification = new MessageTray.Notification({
+      source: systemSource,
+      iconName: iconName,
+      title: this.metadata.name,
+      body: messageText,
+      urgency: mappedUrgency,
+    });
+
+    systemSource.addNotification(notification);
+  }
+
+  _formatTime(totalSeconds) {
+    const totalMinutes = Math.floor(totalSeconds / 60);
+    let seconds = totalSeconds % 60;
+    let hours = Math.floor(totalMinutes / 60);
+    let minutes = totalMinutes % 60;
+
+    if (hours < 10) {
+      hours = "0" + hours;
     }
 
-    _notify() {
-        const messageText = this._settings.get_string("message-text") || "It is time to stretch your back!";
-        const iconName = this._settings.get_string("icon-name") || ICON;
-        const urgencyLevel = this._settings.get_int("urgency-level");
-        const playSound = this._settings.get_boolean("play-sound");
-        const soundName = this._settings.get_string("sound-name") || SOUND;
-
-        const mappedUrgency = urgencyMapping[urgencyLevel];
-
-        const source = new MessageTray.Source(this.metadata.name, iconName);
-        Main.messageTray.add(source);
-        const notification = new MessageTray.Notification(source, this.metadata.name, messageText);
-        notification.setUrgency(mappedUrgency);
-        if (playSound) {
-            notification._soundName = soundName;
-        }
-        source.showNotification(notification);
+    if (minutes < 10) {
+      minutes = "0" + minutes;
     }
 
-    _formatTime(totalSeconds) {
-        const totalMinutes = Math.floor(totalSeconds / 60);
-        let seconds = totalSeconds % 60;
-        let hours = Math.floor(totalMinutes / 60);
-        let minutes = totalMinutes % 60;
+    if (seconds < 10) {
+      seconds = "0" + seconds;
+    }
 
-        if (hours < 10) {
-            hours = "0" + hours;
-        }
+    if (hours > 0) {
+      return `${hours}:${minutes}:${seconds}`;
+    } else {
+      return `${minutes}:${seconds}`;
+    }
+  }
 
-        if (minutes < 10) {
-            minutes = "0" + minutes;
-        }
+  _getTimeLeft() {
+    return this._formatTime(this._timerSec - this._diffSec);
+  }
 
-        if (seconds < 10) {
-            seconds = "0" + seconds;
-        }
+  _addTimer() {
+    this._timer = new PanelMenu.Button(0.0, this.metadata.name, false);
+    this._timerLabel = new St.Label({
+      text: this._getTimeLeft(),
+      y_expand: true,
+      y_align: Clutter.ActorAlign.CENTER,
+    });
+    this._timer.add_child(this._timerLabel);
+    Main.panel.addToStatusArea(this.metadata.name, this._timer);
+  }
 
-        if (hours > 0) {
-            return `${hours}:${minutes}:${seconds}`;
+  _removeTimer() {
+    this._timer.destroy();
+    this._timer = null;
+  }
+
+  _startTimer() {
+    this._timerSec = this._settings.get_int("interval-min") * 60;
+    this._startSec = Math.floor(new Date().getTime() / 1000);
+    this._lastSec = this._startSec;
+    this._diffSec = this._lastSec - this._startSec;
+
+    if (this._settings.get_boolean("show-timer")) {
+      this._addTimer();
+    }
+
+    this._notifyLoop = GLib.timeout_add_seconds(
+      GLib.PRIORITY_DEFAULT,
+      1,
+      () => {
+        this._updateTimer();
+        return GLib.SOURCE_CONTINUE;
+      },
+    );
+  }
+
+  _updateTimer() {
+    this._lastSec = Math.floor(new Date().getTime() / 1000);
+    this._diffSec = this._lastSec - this._startSec;
+
+    if (this._settings.get_boolean("show-timer")) {
+      this._timerLabel.set_text(this._getTimeLeft());
+    }
+
+    if (this._diffSec >= this._timerSec) {
+      this._notify();
+      this._startSec = Math.floor(new Date().getTime() / 1000);
+      this._lastSec = this._startSec;
+    }
+  }
+
+  _stopTimer() {
+    GLib.Source.remove(this._notifyLoop);
+    this._notifyLoop = null;
+    if (this._timer) {
+      this._removeTimer();
+    }
+    this._timerSec = null;
+    this._startSec = null;
+    this._lastSec = null;
+    this._diffSec = null;
+  }
+
+  enable() {
+    this._settings = this.getSettings();
+    // https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/2621
+    this._settings.set_boolean("timer-enabled", false);
+    this._indicator = new DejaviewIndicator(this);
+    this._indicator.quickSettingsItems.push(new DejaviewMenuToggle(this));
+
+    Main.panel.statusArea.quickSettings.addExternalIndicator(this._indicator);
+
+    const autoStart = this._settings.get_boolean("auto-start");
+    if (autoStart) {
+      this._settings.set_boolean("timer-enabled", true);
+      this._startTimer();
+    }
+
+    this._timerEnabledId = this._settings.connect(
+      "changed::timer-enabled",
+      function (_, key) {
+        if (this._settings.get_boolean(key)) {
+          this._startTimer();
         } else {
-            return `${minutes}:${seconds}`;
+          this._stopTimer();
         }
-    }
+      }.bind(this),
+    );
 
-    _getTimeLeft() {
-        return this._formatTime(this._timerSec - this._diffSec);
-    }
-
-    _addTimer() {
-        this._timer = new PanelMenu.Button(0.0, this.metadata.name, false);
-        this._timerLabel = new St.Label({
-            text: this._getTimeLeft(),
-            y_expand: true,
-            y_align: Clutter.ActorAlign.CENTER,
-        });
-        this._timer.add_child(this._timerLabel);
-        Main.panel.addToStatusArea(this.metadata.name, this._timer);
-    }
-
-    _removeTimer() {
-        this._timer.destroy();
-        this._timer = null;
-    }
-
-    _startTimer() {
-        this._timerSec = this._settings.get_int("interval-min") * 60;
-        this._startSec = Math.floor(new Date().getTime() / 1000);
-        this._lastSec = this._startSec;
-        this._diffSec = this._lastSec - this._startSec;
-
-        if (this._settings.get_boolean("show-timer")) {
+    this._showTimerId = this._settings.connect(
+      "changed::show-timer",
+      function (_, key) {
+        if (this._settings.get_boolean("timer-enabled")) {
+          if (this._settings.get_boolean(key)) {
             this._addTimer();
-        }
-
-        this._notifyLoop = GLib.timeout_add_seconds(GLib.PRIORITY_DEFAULT, 1, () => {
-            this._updateTimer();
-            return GLib.SOURCE_CONTINUE;
-        });
-    }
-
-    _updateTimer() {
-        this._lastSec = Math.floor(new Date().getTime() / 1000);
-        this._diffSec = this._lastSec - this._startSec;
-
-        if (this._settings.get_boolean("show-timer")) {
-            this._timerLabel.set_text(this._getTimeLeft());
-        }
-
-        if (this._diffSec >= this._timerSec) {
-            this._notify();
-            this._startSec = Math.floor(new Date().getTime() / 1000);
-            this._lastSec = this._startSec;
-        }
-    }
-
-    _stopTimer() {
-        GLib.Source.remove(this._notifyLoop);
-        this._notifyLoop = null;
-        if (this._timer) {
+          } else {
             this._removeTimer();
+          }
         }
-        this._timerSec = null;
-        this._startSec = null;
-        this._lastSec = null;
-        this._diffSec = null;
+      }.bind(this),
+    );
+
+    // https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/2621
+    this._closingId = global.display.connect("closing", () => {
+      this.disable();
+    });
+  }
+
+  disable() {
+    if (this._notifyLoop) {
+      this._stopTimer();
     }
 
-    enable() {
-        this._settings = this.getSettings();
-        // https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/2621
-        this._settings.set_boolean("timer-enabled", false);
-        this._indicator = new DejaviewIndicator(this);
-        this._indicator.quickSettingsItems.push(new DejaviewMenuToggle(this));
-
-        Main.panel.statusArea.quickSettings.addExternalIndicator(this._indicator);
-
-        const autoStart = this._settings.get_boolean("auto-start");
-        if (autoStart) {
-            this._settings.set_boolean("timer-enabled", true);
-            this._startTimer();
-        }
-
-        this._timerEnabledId = this._settings.connect(
-            "changed::timer-enabled",
-            function (_, key) {
-                if (this._settings.get_boolean(key)) {
-                    this._startTimer();
-                } else {
-                    this._stopTimer();
-                }
-            }.bind(this)
-        );
-
-        this._showTimerId = this._settings.connect(
-            "changed::show-timer",
-            function (_, key) {
-                if (this._settings.get_boolean("timer-enabled")) {
-                    if (this._settings.get_boolean(key)) {
-                        this._addTimer();
-                    } else {
-                        this._removeTimer();
-                    }
-                }
-            }.bind(this)
-        );
-
-        // https://gitlab.gnome.org/GNOME/gnome-shell/-/issues/2621
-        this._closingId = global.display.connect("closing", () => {
-            this.disable();
-        });
-    }
-
-    disable() {
-        if (this._notifyLoop) {
-            this._stopTimer();
-        }
-
-        this._settings.set_boolean("timer-enabled", false);
-        this._settings.disconnect(this._showTimerId);
-        this._settings.disconnect(this._timerEnabledId);
-        this._settings = null;
-        this._showTimerId = null;
-        this._timerEnabledId = null;
-        global.display.disconnect(this._closingId);
-        this._closingId = null;
-        this._indicator.quickSettingsItems.forEach((item) => item.destroy());
-        this._indicator.destroy();
-        this._indicator = null;
-    }
+    this._settings.set_boolean("timer-enabled", false);
+    this._settings.disconnect(this._showTimerId);
+    this._settings.disconnect(this._timerEnabledId);
+    this._settings = null;
+    this._showTimerId = null;
+    this._timerEnabledId = null;
+    global.display.disconnect(this._closingId);
+    this._closingId = null;
+    this._indicator.quickSettingsItems.forEach((item) => item.destroy());
+    this._indicator.destroy();
+    this._indicator = null;
+  }
 }
--- prefs.js
+++ prefs.js
@@ -6,167 +6,160 @@ import Gtk from "gi://Gtk";
 const urgencyLevels = ["low", "normal", "critical"];
 
 export default class DejaviewPreferences extends ExtensionPreferences {
-    fillPreferencesWindow(window) {
-        const settings = this.getSettings();
-        const page = new Adw.PreferencesPage();
-        const groupGeneral = new Adw.PreferencesGroup({ title: "General" });
-        const groupAudio = new Adw.PreferencesGroup({ title: "Audio" });
-        page.add(groupGeneral);
-        page.add(groupAudio);
+  fillPreferencesWindow(window) {
+    const settings = this.getSettings();
+    const page = new Adw.PreferencesPage();
+    const group = new Adw.PreferencesGroup();
+    page.add(group);
 
-        // Auto Start
-        const rowAuto = new Adw.ActionRow({
-            title: "Auto Start",
-            subtitle: "Launch the timer at login and unlock",
-        });
-        groupGeneral.add(rowAuto);
+    // Auto Start
+    const rowAuto = new Adw.ActionRow({
+      title: "Auto Start",
+      subtitle: "Launch the timer at login and unlock",
+    });
+    group.add(rowAuto);
 
-        const toggleAuto = new Gtk.Switch({
-            active: settings.get_boolean("auto-start"),
-            valign: Gtk.Align.CENTER,
-        });
+    const toggleAuto = new Gtk.Switch({
+      active: settings.get_boolean("auto-start"),
+      valign: Gtk.Align.CENTER,
+    });
 
-        settings.bind("auto-start", toggleAuto, "active", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind(
+      "auto-start",
+      toggleAuto,
+      "active",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowAuto.add_suffix(toggleAuto);
-        rowAuto.activatable_widget = toggleAuto;
+    rowAuto.add_suffix(toggleAuto);
+    rowAuto.activatable_widget = toggleAuto;
 
-        // Interval Minutes
-        const rowInterval = new Adw.ActionRow({
-            title: "Interval",
-            subtitle: "Minutes between notifications",
-        });
-        groupGeneral.add(rowInterval);
+    // Interval Minutes
+    const rowInterval = new Adw.ActionRow({
+      title: "Interval",
+      subtitle: "Minutes between notifications",
+    });
+    group.add(rowInterval);
 
-        const adjustment = new Gtk.Adjustment({
-            value: settings.get_int("interval-min"),
-            lower: 1,
-            upper: 1440,
-            step_increment: 1,
-            page_increment: 5,
-        });
+    const adjustment = new Gtk.Adjustment({
+      value: settings.get_int("interval-min"),
+      lower: 1,
+      upper: 1440,
+      step_increment: 1,
+      page_increment: 5,
+    });
 
-        const intervalSpinButton = new Gtk.SpinButton({
-            adjustment,
-            numeric: true,
-            valign: Gtk.Align.CENTER,
-            halign: Gtk.Align.END,
-        });
+    const intervalSpinButton = new Gtk.SpinButton({
+      adjustment,
+      numeric: true,
+      valign: Gtk.Align.CENTER,
+      halign: Gtk.Align.END,
+    });
 
-        settings.bind("interval-min", intervalSpinButton.get_adjustment(), "value", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind(
+      "interval-min",
+      intervalSpinButton.get_adjustment(),
+      "value",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowInterval.add_suffix(intervalSpinButton);
-        rowInterval.activatable_widget = intervalSpinButton;
+    rowInterval.add_suffix(intervalSpinButton);
+    rowInterval.activatable_widget = intervalSpinButton;
 
-        // Message Text
-        const rowMessage = new Adw.ActionRow({
-            title: "Message Text",
-            subtitle: "Custom notification",
-        });
-        groupGeneral.add(rowMessage);
+    // Message Text
+    const rowMessage = new Adw.ActionRow({
+      title: "Message Text",
+      subtitle: "Custom notification",
+    });
+    group.add(rowMessage);
 
-        const messageEntry = new Gtk.Entry({
-            placeholder_text: "It is time to stretch your back!",
-            text: settings.get_string("message-text"),
-            valign: Gtk.Align.CENTER,
-            hexpand: true,
-        });
+    const messageEntry = new Gtk.Entry({
+      placeholder_text: "It is time to stretch your back!",
+      text: settings.get_string("message-text"),
+      valign: Gtk.Align.CENTER,
+      hexpand: true,
+    });
 
-        settings.bind("message-text", messageEntry, "text", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind(
+      "message-text",
+      messageEntry,
+      "text",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowMessage.add_suffix(messageEntry);
-        rowMessage.activatable_widget = messageEntry;
+    rowMessage.add_suffix(messageEntry);
+    rowMessage.activatable_widget = messageEntry;
 
-        // Icon Name
-        const rowIcon = new Adw.ActionRow({
-            title: "Icon Name",
-            subtitle: "/usr/share/icons/",
-        });
-        groupGeneral.add(rowIcon);
+    // Icon Name
+    const rowIcon = new Adw.ActionRow({
+      title: "Icon Name",
+      subtitle: "/usr/share/icons/",
+    });
+    group.add(rowIcon);
 
-        const iconEntry = new Gtk.Entry({
-            placeholder_text: "alarm-symbolic",
-            text: settings.get_string("icon-name"),
-            valign: Gtk.Align.CENTER,
-            hexpand: true,
-        });
+    const iconEntry = new Gtk.Entry({
+      placeholder_text: "alarm-symbolic",
+      text: settings.get_string("icon-name"),
+      valign: Gtk.Align.CENTER,
+      hexpand: true,
+    });
 
-        settings.bind("icon-name", iconEntry, "text", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind(
+      "icon-name",
+      iconEntry,
+      "text",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowIcon.add_suffix(iconEntry);
-        rowIcon.activatable_widget = iconEntry;
+    rowIcon.add_suffix(iconEntry);
+    rowIcon.activatable_widget = iconEntry;
 
-        // Urgency Level
-        const rowUrgency = new Adw.ActionRow({
-            title: "Urgency Level",
-            subtitle: "Critical ones are auto-expanded but get dismissed only after a click",
-        });
-        groupGeneral.add(rowUrgency);
+    // Urgency Level
+    const rowUrgency = new Adw.ActionRow({
+      title: "Urgency Level",
+      subtitle:
+        "Critical ones are auto-expanded but get dismissed only after a click",
+    });
+    group.add(rowUrgency);
 
-        const dropdownUrgency = new Gtk.DropDown({
-            valign: Gtk.Align.CENTER,
-            model: Gtk.StringList.new(urgencyLevels),
-            selected: settings.get_string("urgency-level"),
-        });
+    const dropdownUrgency = new Gtk.DropDown({
+      valign: Gtk.Align.CENTER,
+      model: Gtk.StringList.new(urgencyLevels),
+      selected: settings.get_string("urgency-level"),
+    });
 
-        settings.bind("urgency-level", dropdownUrgency, "selected", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind(
+      "urgency-level",
+      dropdownUrgency,
+      "selected",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowUrgency.add_suffix(dropdownUrgency);
-        rowUrgency.activatable_widget = dropdownUrgency;
+    rowUrgency.add_suffix(dropdownUrgency);
+    rowUrgency.activatable_widget = dropdownUrgency;
 
-        // Show Timer
-        const rowTimer = new Adw.ActionRow({
-            title: "Show Timer",
-            subtitle: "Display a timer countdown in panel",
-        });
-        groupGeneral.add(rowTimer);
+    // Show Timer
+    const rowTimer = new Adw.ActionRow({
+      title: "Show Timer",
+      subtitle: "Display a timer countdown in panel",
+    });
+    group.add(rowTimer);
 
-        const toggleTimer = new Gtk.Switch({
-            active: settings.get_boolean("show-timer"),
-            valign: Gtk.Align.CENTER,
-        });
+    const toggleTimer = new Gtk.Switch({
+      active: settings.get_boolean("show-timer"),
+      valign: Gtk.Align.CENTER,
+    });
 
-        settings.bind("show-timer", toggleTimer, "active", Gio.SettingsBindFlags.DEFAULT);
+    settings.bind(
+      "show-timer",
+      toggleTimer,
+      "active",
+      Gio.SettingsBindFlags.DEFAULT,
+    );
 
-        rowTimer.add_suffix(toggleTimer);
-        rowTimer.activatable_widget = toggleTimer;
+    rowTimer.add_suffix(toggleTimer);
+    rowTimer.activatable_widget = toggleTimer;
 
-        // Play Sound
-        const rowAlert = new Adw.ActionRow({
-            title: "Play Sound",
-            subtitle: "Add audio alert to notifications",
-        });
-        groupAudio.add(rowAlert);
-
-        const toggleAlert = new Gtk.Switch({
-            active: settings.get_boolean("play-sound"),
-            valign: Gtk.Align.CENTER,
-        });
-
-        settings.bind("play-sound", toggleAlert, "active", Gio.SettingsBindFlags.DEFAULT);
-
-        rowAlert.add_suffix(toggleAlert);
-        rowAlert.activatable_widget = toggleAlert;
-
-        // Sound Name
-        const rowSound = new Adw.ActionRow({
-            title: "Sound Name",
-            subtitle: "/usr/share/sounds/freedesktop/stereo/",
-        });
-        groupAudio.add(rowSound);
-
-        const soundEntry = new Gtk.Entry({
-            placeholder_text: "complete",
-            text: settings.get_string("sound-name"),
-            valign: Gtk.Align.CENTER,
-            hexpand: true,
-        });
-
-        settings.bind("sound-name", soundEntry, "text", Gio.SettingsBindFlags.DEFAULT);
-
-        rowSound.add_suffix(soundEntry);
-        rowSound.activatable_widget = soundEntry;
-
-        window.add(page);
-    }
+    window.add(page);
+  }
 }
